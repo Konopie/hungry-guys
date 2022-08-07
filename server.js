50f9154957ee073132531494991a4fa4ff58a5a0
Cody Cooper <codycooper06@gmail.com>
2022-08-07 11:33:12 -0400

    basic handlebars

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 //create app and initialize session variables
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// create view engine for handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>{
+    res.render('home');
+})
 
 
 //turn on the connection to the db server
